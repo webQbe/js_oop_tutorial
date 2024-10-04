f170bee58043073dcf51b527053cd6e3dec6cd74
webQbe <webcubesolutionslk@gmail.com>
2024-10-04 16:28:04 +0530

    Object Literals

--- 1_basics_literals.js
+++ 1_basics_literals.js
@@ -4,7 +4,7 @@
 const s = 'Hello';
 console.log(s.toUpperCase()); */
 
-// Strings and Objects
+/* // Strings and Objects
 
 // log s1 type
 const s1 = 'Hello';
@@ -22,8 +22,35 @@ console.log(window);
 console.log(window.alert(1));
 
 // window with object and its property
-console.log(window.navigator.appVersion);
+console.log(window.navigator.appVersion);*/
 
+// Object Literals
+
+// book1 object
+const book1 = {
+    title: 'Book One',
+    author: 'John Doe',
+    year: '2013',
+
+    // add a method to object
+    getSummary: function(){
+        return `${this.title} was written by ${this.author} in ${this.year}.`;
+    }
+};
+
+console.log(book1);
+
+// get property from object
+console.log(book1.title);
+
+// get method from object
+console.log(book1.getSummary());
+
+// get object values
+console.log(Object.values(book1));
+
+// get object keys
+console.log(Object.keys(book1));
 
 
 
