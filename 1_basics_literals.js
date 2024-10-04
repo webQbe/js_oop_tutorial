139dcd46c358302a4cbd225d631132bb0a647b65
webQbe <webcubesolutionslk@gmail.com>
2024-10-04 16:23:12 +0530

    Strings and Objects

--- 1_basics_literals.js
+++ 1_basics_literals.js
@@ -1,8 +1,30 @@
 
-// Object with method
+/* // Object with method
 
 const s = 'Hello';
-console.log(s.toUpperCase());
+console.log(s.toUpperCase()); */
+
+// Strings and Objects
+
+// log s1 type
+const s1 = 'Hello';
+console.log(typeof s1);
+
+// log s2 type
+const s2 = new String('Hello');
+console.log(typeof s2);
+
+
+// window is the Parent object of DOM
+console.log(window);
+
+// window with method
+console.log(window.alert(1));
+
+// window with object and its property
+console.log(window.navigator.appVersion);
+
+
 
 
 
