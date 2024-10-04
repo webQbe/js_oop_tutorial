af9b670437fc479924d2535cc46c9f2793538a56
webQbe <webcubesolutionslk@gmail.com>
2024-10-04 16:50:51 +0530

    Constructors

--- oop.js
+++ oop.js
@@ -24,7 +24,7 @@ console.log(window.alert(1));
 // window with object and its property
 console.log(window.navigator.appVersion);*/
 
-// Object Literals
+/* // Object Literals
 
 // book1 object
 const book1 = {
@@ -54,5 +54,36 @@ console.log(Object.keys(book1));
 
 
 
+ */
 
 
+// Constructors
+
+// If we want to duplicate objects we can use constructors
+// constructor is a function
+
+function Book(title, author, year){
+    this.title = title;
+    this.author = author;
+    this.year = year;
+
+    this.getSummary = function(){
+        return `${this.title} was written by ${this.author} in ${this.year}.`;
+    }
+}
+
+// Instantiating objects
+const book1 = new Book('Book One', 'John Doe', '2013');
+const book2 = new Book('Book Two', 'Jane Doe', '2016');
+
+// display book1 object
+console.log(book1);
+
+// get title of book1
+console.log(book1.title);
+
+// get summary of book1
+console.log(book1.getSummary());
+
+// get summary of book2
+console.log(book2.getSummary());
